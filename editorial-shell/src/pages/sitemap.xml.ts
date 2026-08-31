71bf39e8eb966c222b0f6a734cda933b1944aebc
Renan Melo <byrenanmelo@gmail.com>
2026-08-31 08:56:40 -0300

    R2.2 emit physical sitemap

---
+++ editorial-shell/src/pages/sitemap.xml.ts
@@ -0,0 +1,6 @@
+import type { APIRoute } from 'astro';
+import { distributionArtifactResponse } from '../lib/artifact-response';
+
+export const prerender = true;
+
+export const GET: APIRoute = () => distributionArtifactResponse('/sitemap.xml');
