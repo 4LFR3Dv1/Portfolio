4cba95bf7a576df1ac6dcc0d94f27c305a93ad3c
Renan Melo <byrenanmelo@gmail.com>
2026-08-31 20:56:14 -0300

    R3.0 expose editorial domain to Astro

---
+++ editorial-shell/publication-src/data/editorial.ts
@@ -0,0 +1,31 @@
+export {
+  editorialInquiries,
+  getEditorialInquiry,
+  getInquiryHref,
+  getInquiryPublicationState,
+} from '../../../src/app/data/editorial-inquiries';
+
+export {
+  editorialThreads,
+  getEditorialThread,
+} from '../../../src/app/data/editorial-threads';
+
+export {
+  editorialState,
+  getActiveInquiries,
+  getActiveThreads,
+  getPrimaryInquiry,
+  getPublicationEditorialContext,
+  getThreadInquiries,
+} from '../../../src/app/data/editorial-state';
+
+export type {
+  EditorialInquiry,
+  EditorialInquiryId,
+  EditorialInquiryState,
+} from '../../../src/app/data/editorial-inquiries';
+
+export type {
+  EditorialThread,
+  EditorialThreadId,
+} from '../../../src/app/data/editorial-threads';
