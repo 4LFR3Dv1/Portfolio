f3499bf5310ef30465f8997564c9e665b74555b4
Renan Melo <byrenanmelo@gmail.com>
2026-09-03 16:08:41 -0300

    Aggregate Editorial Cohort 003

--- src/app/data/editorial-publications-all.ts
+++ src/app/data/editorial-publications-all.ts
@@ -4,10 +4,12 @@ import {
   type PublicationKind,
 } from './editorial-publications';
 import { cohort002Publications } from './editorial-publications-cohort-002';
+import { cohort003Publications } from './editorial-publications-cohort-003';
 
 export const publications: EditorialPublication[] = [
   ...cohort001Publications,
   ...cohort002Publications,
+  ...cohort003Publications,
 ];
 
 export const featuredPublications = publications.filter((publication) => publication.featured);
